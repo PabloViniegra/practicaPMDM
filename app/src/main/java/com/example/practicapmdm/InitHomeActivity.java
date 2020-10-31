14eee22ae7329a950bd8bee3baf8652d165d3777
Your Name <you@example.com>
2020-10-31 01:50:35 +0100

    Modificado Navigation View de nuevo

---
+++ app/src/main/java/com/example/practicapmdm/InitHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.practicapmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_home);
+    }
+}
