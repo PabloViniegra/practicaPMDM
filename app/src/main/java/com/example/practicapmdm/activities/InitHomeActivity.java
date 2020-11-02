605ef5c71b19b14ce2179471869c3983c786e3b9
Pablo Viniegra <pablovpmadrid@gmail.com>
2020-11-02 14:48:30 +0100

    Avance en el Navigation View. Los xml están construidos. Falta implementar la lógica en Java.

--- app/src/main/java/com/example/practicapmdm/activities/InitHomeActivity.java
+++ app/src/main/java/com/example/practicapmdm/activities/InitHomeActivity.java
@@ -1,16 +1,51 @@
 package com.example.practicapmdm.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.practicapmdm.R;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
-public class InitHomeActivity extends AppCompatActivity {
+public class InitHomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init_home);
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                                                                        drawer,
+                                                                        toolbar,
+                                                                        R.string.navigation_drawer_open,
+                                                                        R.string.navigation_drawer_close);
+
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
