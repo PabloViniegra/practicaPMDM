232de159534f89a0ce1ded5d5afff5d5416ea41a
Your Name <you@example.com>
2020-11-04 01:01:42 +0100

    Avances en la navegación. La lógica ha sido implementada en parte. También se han construido parte de los seervicios de GPS y del Activity dedicado a la construcción del OpenStreetMaps

--- app/src/main/java/com/example/practicapmdm/activities/MapsActivity.java
+++ app/src/main/java/com/example/practicapmdm/activities/MapsActivity.java
@@ -2,15 +2,63 @@ package com.example.practicapmdm.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.example.practicapmdm.R;
 
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
+import static com.example.practicapmdm.constants.Constants.LONGITUDE;
+import static com.example.practicapmdm.constants.Constants.LATITUDE;
+import static com.example.practicapmdm.activities.InitHomeActivity.TITLE_KEY;
+import static com.example.practicapmdm.activities.InitHomeActivity.TITLE;
+import static com.example.practicapmdm.activities.InitHomeActivity.DESCRIPTION_KEY;
+import static com.example.practicapmdm.activities.InitHomeActivity.DESCRIPTION;
+
 public class MapsActivity extends AppCompatActivity {
+    private MapView mMap;
+    private MapController mapController;
+    GeoPoint geoMyPosition;
+    private ArrayList <OverlayItem> overlayItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
+
+        Intent getDataIntent = getIntent();
+        geoMyPosition = new GeoPoint(getDataIntent.getDoubleExtra(LATITUDE, 0), getDataIntent.getDoubleExtra(LONGITUDE, 0));
+        generateOpenMaps();
+
+        boolean add = overlayItems.add(new OverlayItem(getDataIntent.getStringExtra(TITLE_KEY), getDataIntent.getStringExtra(DESCRIPTION_KEY),geoMyPosition));
+        ItemizedOverlayWithFocus<OverlayItem> mOverLay = new ItemizedOverlayWithFocus<OverlayItem>(overlayItems, new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+            @Override
+            public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                return false;
+            }
+
+            @Override
+            public boolean onItemLongPress(int index, OverlayItem item) {
+                return false;
+            }
+        },getApplicationContext());
+
+        mOverLay.setFocusItemsOnTap(true);
+        mMap.getOverlays().add(mOverLay);
+    }
+
+    public void generateOpenMaps () {
+
     }
 }
