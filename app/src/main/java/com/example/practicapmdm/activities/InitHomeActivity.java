2c00d1d8c85a3fb214b6dd3e2ee9e5376961014a
Your Name <you@example.com>
2020-11-07 17:45:06 +0100

    Agregado método de carga al principio de la Aplicación. Por testear

--- app/src/main/java/com/example/practicapmdm/activities/InitHomeActivity.java
+++ app/src/main/java/com/example/practicapmdm/activities/InitHomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,6 +16,11 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.example.practicapmdm.R;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static com.example.practicapmdm.constants.Constants.LATITUDE;
 import static com.example.practicapmdm.constants.Constants.LONGITUDE;
@@ -38,7 +44,7 @@ public class InitHomeActivity extends AppCompatActivity implements NavigationVie
 
         Toolbar toolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.btn_moreinfo);
+        Objects.requireNonNull(getSupportActionBar()).setHomeAsUpIndicator(R.drawable.btn_moreinfo);
         drawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
                                                                         drawerLayout,
@@ -110,5 +116,43 @@ public class InitHomeActivity extends AppCompatActivity implements NavigationVie
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+
+    public void loading() {
+        int i = 100;
+        final int[] n = {0};
+        Random r = new Random();
+        int random = r.nextInt(1);
+        final TextView percent = null;
+        final int[] opcion_0 = {1, 20, 40, 60, 89, 100};
+        int[] opcion_1 = {1, 30, 60, 90, 99, 100};
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        switch (random) {
+            case 0:
+                // con el métod submit() se envía la tarea que se hará en el nuevo hilo
+                // hará una pausa de un segundo ( al hilo secundario ) y actualizará
+                // el valor del elemento de la UI
+                executor.submit(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        for (int k = 0; k <= 5; k++) {
+                            String temp = String.valueOf(opcion_0[n[0]]);
+                            assert percent != null;
+                            percent.setText(temp);
+                            n[0]++;
+
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+                break;
+
+        }
     }
 }
