d8a87cd15a32f02369154a8cc2ac159e9cd2124c
Jonatandlfuente <jonatan.delafuente@juanxxiii.net>
2020-11-30 22:26:36 +0100

    arreglados problemas de carga, creacion de la clase FileController

---
+++ app/src/main/java/com/example/practicapmdm/models/Favourites.java
@@ -0,0 +1,17 @@
+package com.example.practicapmdm.models;
+
+public class Favourites {
+    private Pool pool;
+
+    public Favourites(Pool pool) {
+        this.pool = pool;
+    }
+
+    public Pool getPool() {
+        return pool;
+    }
+
+    public void setPool(Pool pool) {
+        this.pool = pool;
+    }
+}
